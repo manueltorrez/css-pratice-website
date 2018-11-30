2dea33eb8a26c1cf81938b6c12ee7e40684305a2
Manuel Torrez <manuel.torrez.vado@protonmail.com>
2018-11-30 09:53:32 -0600

    Adding a side navbar

--- shared.js
+++ shared.js
@@ -2,6 +2,8 @@ let backdrop = document.querySelector('.backdrop');
 let modal = document.querySelector('.modal');
 let modalNoButton = document.querySelector('.modal__action--negative')
 let selectPlanButtons = document.querySelectorAll('.plan button');
+let toggleButton = document.querySelector('.toggle-button');
+let mobileNav = document.querySelector('.mobile-nav');
 
 let openModal = () => {
     backdrop.style.display = 'block';
@@ -13,8 +15,20 @@ let closeModal = () => {
     modal.style.display = 'none';
 }
 
+let showMobileNav = () => {
+    mobileNav.style.display = 'block';
+    backdrop.style.display = 'block';
+}
+
 for(el of selectPlanButtons) {
     el.addEventListener('click', openModal);
 }
 
 modalNoButton.addEventListener('click', closeModal);
+
+toggleButton.addEventListener('click', showMobileNav);
+
+backdrop.addEventListener('click', () => {
+    closeModal();
+    mobileNav.style.display = 'none';
+});
